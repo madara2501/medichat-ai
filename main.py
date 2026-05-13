79f2cc12c3b6d12c8e9df9fbb637551d58e775a8
Pabitra jana <pabitrajana2020@gmail.com>
2026-05-13 14:43:19 +0530

    Enhance email handling and OTP functionality
    Added mail configuration settings and improved OTP handling in the registration and password reset processes.

--- main.py
+++ main.py
@@ -19,11 +19,17 @@ app = Flask(__name__)
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
+
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 
+app.config['MAIL_DEBUG'] = True
+app.config['MAIL_SUPPRESS_SEND'] = False
+
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
 mail = Mail(app) 
 
 app.secret_key = os.getenv("SECRET_KEY")
