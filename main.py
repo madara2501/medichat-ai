08e99f65f3713450ac481f49c8473750db9eb2f7
Pabitra jana <pabitrajana2020@gmail.com>
2026-05-13 13:42:48 +0530

    Fix Gmail SMTP configuration

--- main.py
+++ main.py
@@ -20,8 +20,10 @@ app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
-app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
+app.config['MAIL_USE_SSL'] = False
+
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 mail = Mail(app) 
 
 app.secret_key = os.getenv("SECRET_KEY")
