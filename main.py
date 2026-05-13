103e34b2fc9ab14536345099d37eb3cc1cc2d146
Pabitra jana <pabitrajana2020@gmail.com>
2026-05-13 15:18:36 +0530

    Fix email configuration and update OTP handling

--- main.py
+++ main.py
@@ -18,13 +18,11 @@ load_dotenv()
 app = Flask(__name__)
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+
 app.config['MAIL_PORT'] = 465
 
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
-
-app.config['MAIL_DEBUG'] = True
-app.config['MAIL_SUPPRESS_SEND'] = False
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
